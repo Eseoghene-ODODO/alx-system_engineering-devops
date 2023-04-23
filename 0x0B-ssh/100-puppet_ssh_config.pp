014c93e77f5c3a3f4e135c46b4159a0f6d81061c
Eseoghene-ODODO <en.ododohotmail.com>
2023-04-23 13:52:45 -0700

    solution to task 0-4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,7 +3,7 @@
 require 'net/ssh'
 
 ssh_key = File.read("#{ENV['HOME']}/.ssh/school")
-ssh_host = '100.25.38.184'
+ssh_host = '100.25.201.12'
 ssh_user = 'ubuntu'
 
 Net::SSH.start(ssh_host, ssh_user, keys: [ssh_key]) do |ssh|
