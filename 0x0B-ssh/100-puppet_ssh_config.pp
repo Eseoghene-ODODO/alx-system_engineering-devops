45b462df4b658af69d3a03ae5607b4cd381c380e
Eseoghene-ODODO <en.ododohotmail.com>
2023-04-23 20:39:28 -0700

    solution to task 0-4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 # Client configuration file (w/ Puppet)
 
-file { '/home/root/.ssh/config':
-  ensure  => file,
-  owner   => 'root,
-  group   => root',
-  mode    => '0644',
+file { '/root/.ssh/config':
+  ensure  => 'present',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0600',
   content => "
     Host 35.175.132.186
       IdentityFile ~/.ssh/school
