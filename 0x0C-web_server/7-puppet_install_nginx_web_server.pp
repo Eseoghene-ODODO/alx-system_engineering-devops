4eddd1a6b94c295e88361403097dcd7ef4961a4e
Eseoghene-ODODO <en.ododohotmail.com>
2023-05-08 06:41:57 -0700

    0-5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,29 +1,37 @@
-#!/usr/bin/env bash
 # A script that Install Nginx web server (w/ Puppet)
 
 
-# Create a Puppet manifest file
-cat <<EOF > nginx_manifest.pp
-class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  file { '/etc/nginx/sites-enabled/default':
-    ensure  => file,
-    content => "server {\n  listen 80;\n  server_name _;\n  location / {\n    return 200 'Hello World!';\n  }\n  location /redirect_me {\n    return 301 https://one-techschool.tech;\n  }\n}\n",
-    require => Package['nginx'],
-  }
-
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => File['/etc/nginx/sites-enabled/default'],
-  }
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
 }
 
-class { 'nginx': }
-EOF
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
 
-# Apply the Puppet manifest
-sudo puppet apply nginx_manifest.pp
+# Configure Nginx to listen on port 80
+file { '/etc/nginx/sites-available/default':
+  ensure => present,
+  content => "
+    server {
+      listen 80;
+      server_name _;
+      
+      location / {
+        return 200 'Hello World!';
+      }
+
+      location /redirect_me {
+        return 301 /redirected;
+      }
+
+      location /redirected {
+        return 200 'You have been redirected.';
+      }
+    }
+  ",
+  notify => Service['nginx'],
+}
