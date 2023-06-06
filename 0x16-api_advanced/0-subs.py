80bcca51b2d4d9a4ce7cb6c6de1c92d46141c114
Eseoghene-ODODO <en.ododo@gmail.com>
2023-06-06 14:14:45 -0700

    API advance task solution

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
+If an invalid subreddit is given, the function returns 0.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Retrieves the number of subscribers for a given subreddit.
+    Returns 0 if the subreddit is invalid.
+    """
+    # Set a custom User-Agent header
+    headers = {'User-Agent': 'My Reddit Client'}
+
+    # Make the API request
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    subreddit = 'python'
+    subscribers = number_of_subscribers(subreddit)
+    print(f"The subreddit r/{subreddit} has {subscribers} subscribers.")
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+If not a valid subreddit, prints None.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+    If the subreddit is invalid, prints None.
+    """
+    # Set a custom User-Agent header
+    headers = {'User-Agent': 'My Reddit Client'}
+
+    # Make the API request
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            title = post['data']['title']
+            print(title)
+    else:
+        print(None)
+
+
+if __name__ == '__main__':
+    subreddit = 'python'
+    top_ten(subreddit)
