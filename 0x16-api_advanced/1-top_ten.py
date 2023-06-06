c1e82c6a69a99b80671f6061298001d8bc6f924c
Eseoghene-ODODO <en.ododo@gmail.com>
2023-06-06 14:48:35 -0700

    API advance task solution

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,10 +10,16 @@ def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit"""
     # Set the custom User-Agent header
     headers = {"User-Agent": "Bing"}
-    # Make a GET request to the subreddit's hot.json endpoint with a limit of 10
+    # Make a GET request to the subreddit's hot.json endpoint
+    # with a limit of 10
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     params = {"limit": 10}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False
+            )
     # Check the status code and the response data
     if response.status_code == 200:
         data = response.json().get("data")
