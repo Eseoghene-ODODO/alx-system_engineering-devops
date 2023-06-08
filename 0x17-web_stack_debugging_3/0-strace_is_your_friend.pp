797c16963792af696a54ffccebd48e8fde8af90a
Eseoghene-ODODO <en.ododo@gmail.com>
2023-06-08 15:20:38 -0700

    webstack debbuging solution

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,20 +1,6 @@
-# This manifest ensures that the wp-settings.php file for WordPress is present and has the correct content and permissions
-
-# Declare the apache module
-include apache
-
-# Manage the wp-settings.php file
-package { 'apache2':
-  ensure => installed,
-  # Add any additional package attributes here
-}
+# Puppet code to fix wordpress configuration file in apache2 error
 
 file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  source  => 'puppet:///modules/apache/wp-settings.php',
-  require => Package['apache2'], # Require the apache2 package
-  notify  => Service['apache2'], # Notify the apache2 service
+  ensure  => present,
+  content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub(/\.phpp/, ".php") %>'),
 }
