fde4038dca505f6de4d1a5e8773f2542ccb6ae37
Eseoghene-ODODO <en.ododo@gmail.com>
2023-06-08 02:13:16 -0700

    solution

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,15 @@
 # This manifest ensures that the wp-settings.php file for WordPress is present and has the correct content and permissions
 
+# Declare the apache module
+include apache
 
+# Manage the wp-settings.php file
 file { '/var/www/html/wp-settings.php':
   ensure  => file,
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0644',
   source  => 'puppet:///modules/apache/wp-settings.php',
-  require => Package['apache2'],
-  notify  => Service['apache2'],
+  require => Package['apache2'], # Require the apache2 package
+  notify  => Service['apache2'], # Notify the apache2 service
 }
