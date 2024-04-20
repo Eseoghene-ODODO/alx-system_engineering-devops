b6385961830d10a7ffaa9115be2261122068cd0e
Eseoghene-ODODO <en.ododo@gmail.com>
2024-04-20 05:18:05 +0000

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,8 +4,7 @@ package { 'python3-pip':
   ensure => 'present',
 }
 
-python::pip { 'flask':
-  ensure   => 'present', 
+python::pip { 'flask': 
   version => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
