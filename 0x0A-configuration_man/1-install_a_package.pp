d34fa25a7ed3cfbcbc8b5f09a214d800da661547
Eseoghene-ODODO <en.ododo@gmail.com>
2024-04-20 04:39:18 +0000

    task 1 updated

--- 0x0A-configuration_man/1-install_a_package.pp
+++ 0x0A-configuration_man/1-install_a_package.pp
@@ -1,10 +1,12 @@
-# Using Puppet, install flask from pip3.
+# This Puppet code installs Flask version 2.1.0 using pip3
+
 package { 'python3-pip':
-  ensure => present,
+  ensure => 'present',
 }
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+python::pip { 'flask':
+  ensure     => 'present',
+  pkgname    => 'flask',
+  version    => '2.1.0',
+  require    => Package['python3-pip'],
 }
