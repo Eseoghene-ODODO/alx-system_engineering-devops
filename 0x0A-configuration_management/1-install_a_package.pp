6c5ca7dbc2098a6b53da76cd188f9ed04971103b
Eseoghene-ODODO <en.ododo@gmail.com>
2024-04-20 06:38:22 +0000

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,11 @@
 # This Puppet code installs Flask version 2.1.0 using pip3
 
 package { 'python3-pip':
-  ensure => 'present',
+  ensure => 'installed',
 }
 
-Package { 'flask':
-  ensure => 'installed',
-  version => '2.1.0',
+exec { 'install-flask':
+  command => 'pip3 install flask==2.1.0',
   provider => 'pip3',
   path => ['/usr/bin'],
   require  => Package['python3-pip'],
